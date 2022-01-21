f0b0fa5943584107476ff1f2c214bdf0e46a98d1
Oliver Knoll <starsky.hutch@gmx.ch>
2022-01-21 20:29:23 +0100

    Add call ctor from ctor code example

---
+++ src/language/2.10-ctor-from-ctor/README.md
@@ -0,0 +1,22 @@
+# Call constructor from constructor
+
+## About
+A constructor may delegate part of the class initialisation to another constructor, by simply
+calling that other constructor.
+
+## Why
+Instead of calling a common "initialise" function calling another constructor is cleaner and
+still profits from direct member initialisation.
+
+As a side effect: once the first constructor is finished the class is "completely initialised"
+(except the additional member variables), which means that when deriving from a base class
+and calling its base constructor the current constructor may then already call virtual methods
+(which is possible as soon as at least one constructor has been fully completed).
+
+## How to use
+Having lots of different constructors may be confusing and constructors may call each other if
+one does not pay attention. To prevent just this good practice is that constructors with less
+arguments (only) call those constructors with more arguments (by providing default values).
+
+## Since
+C++11
---
+++ src/language/2.10-ctor-from-ctor/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <memory>
+
+class Base
+{
+public:
+    Base(int a, int b, int c)
+        : a_(a), b_(b), c_(c), d_(1001)
+    {}
+
+    /*!
+     * One constructor may call another constructor. As a good practice
+     * constructors with fewer parameters should call those with more
+     * parameters, in order to avoid confusion and possibly even mutual
+     * dependency (endless recursion of constructor calls).
+     */
+    Base(int a, int b)
+        : Base(a, b, 42)
+    {}
+
+    // Note that the compiler expects a constructor call to completely
+    // initialise the object, so a mix with other direct initialisers
+    // is not possible. So the following would not compile:
+    // "an initializer for a delegating constructor must appear alone"
+//    Base(int a, int b)
+//        : Base(a, b, 42), d_(42)
+//    {}
+
+    virtual ~Base() = default;
+
+protected:
+    virtual void someMethod()
+    {
+        std::cout << "Base::someMethod called." << std::endl;
+    }
+
+private:
+    [[maybe_unused]]
+    int a_;
+    [[maybe_unused]]
+    int b_;
+    [[maybe_unused]]
+    int c_;
+    [[maybe_unused]]
+    int d_;
+};
+
+class Derived : public Base
+{
+public:
+    /*!
+     * We may also call the base class constructor.
+     */
+    Derived(int a, int b)
+        : Base(a, b)
+    {
+        // As soon as one constructor has been completed - here: Base() - we
+        // may start to call virtual methods. Even in this constructor here.
+        // Note: some code analysers such as the clang-analyzer may still complain that a
+        //       "call to virtual method during construction bypasses virtual dispatch".
+        //       However the proper Derived::someMethod() method is still being called.
+        //
+        //       Whether that is good practice is another question then.
+        someMethod();
+    }
+
+protected:
+    virtual void someMethod() override
+    {
+        std::cout << "Derived::someMethod called." << std::endl;
+    }
+};
+
+int main(int, char *[])
+{
+    std::unique_ptr<Base> d = std::make_unique<Derived>(1, 2);
+    return 0;
+}
